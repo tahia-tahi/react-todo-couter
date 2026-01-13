f61c080cc3e51dc5cf6e7112ba7f133999cf163a
tahia-tahi <devpro526@gmail.com>
2026-01-13 11:45:51 +0600

    created basic react app

---
+++ src/Counter.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Counter</h2>
+      <p>Value: {count}</p>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
